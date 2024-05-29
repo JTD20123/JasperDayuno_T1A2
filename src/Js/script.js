27b2a3768837bdd9f971a69b1a918df7091e2eff
JTD20123 <jasperdayuno@yahoo.com>
2024-05-29 17:20:15 +1000

    Pages linked

--- src/Js/script.js
+++ src/Js/script.js
@@ -5,8 +5,3 @@ function toggleMenu() {
     icon.classList.toggle("open");
 }
 
-$(".bar").each(function(){
-    $(this).find(".bar-inner").animate({
-      width: $(this).attr("data-width")
-    },2000)
-  });
